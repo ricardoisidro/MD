d421a8d4ba4305a662853f7dcbc7fdb26333c469
ricardo <ricardo.isidro.86@gmail.com>
2018-10-11 10:18:56 -0500

    Initial Commit

---
+++ Maguen/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Maguen
+//
+//  Created by Ricardo Isidro on 10/11/18.
+//  Copyright © 2018 Expression B. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Maguen/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Maguen
+//
+//  Created by Ricardo Isidro on 10/11/18.
+//  Copyright © 2018 Expression B. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
