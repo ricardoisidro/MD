a3097e493af513a931ef65945a177f458fa74476
ExpresionBinaria <expressionb@expresionbinaria.local>
2018-10-29 12:39:22 -0600

    29/10/18

---
+++ Maguen/SettingsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsTableViewController.swift
+//  Maguen
+//
+//  Created by ExpresionBinaria on 10/19/18.
+//  Copyright © 2018 Expression B. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.backgroundColor = MaguenColors.black1
+        
+    }
+    
+}
